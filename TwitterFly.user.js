119648da026ff341205b24b449738236932c088c
Pavel <me@xpaw.ru>
2012-02-11 16:43:37 +0200

    Added TWITTER_PAGE_WIDTH. Changed @include to @match for more strict url matching. Removed anonymous function wrapper.

--- TwitterFly.user.js
+++ TwitterFly.user.js
@@ -3,24 +3,24 @@
 // @version        1.0
 // @namespace      xPaw
 // @description    960px content, tweets on the left side
-// @include        https://twitter.com/*
+// @match          https://twitter.com/*
 // @run-at         document-end
 // ==/UserScript==
 
-( function( ) {
-	var AddStyles = function( css )
-	{
-		var node = document.createElement( 'style' );
-		node.type = 'text/css';
-		node.appendChild( document.createTextNode( css ) );
-		
-		document.getElementsByTagName( 'head' )[ 0 ].appendChild( node ); 
-	};
+var TWITTER_PAGE_WIDTH = 932;
+
+var AddStyles = function( css )
+{
+	var node = document.createElement( 'style' );
+	node.type = 'text/css';
+	node.appendChild( document.createTextNode( css ) );
 	
-	AddStyles(
-		'.wrapper { width: 960px }' +
-		'.wrapper-narrow, .wrapper-permalink { width: 630px }' +
-		'.dashboard { float: right }' +
-		'.content-main { float: left; width: 645px }'
-	);
-} )( );
+	document.getElementsByTagName( 'head' )[ 0 ].appendChild( node );
+};
+
+AddStyles(
+	'.wrapper { width: ' + TWITTER_PAGE_WIDTH + 'px }' +
+	'.wrapper-narrow, .wrapper-permalink { width: 630px }' +
+	'.dashboard { float: right }' +
+	'.content-main { float: left; width: ' + ( TWITTER_PAGE_WIDTH - 315 ) + 'px }'
+);
